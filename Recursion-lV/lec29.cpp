42b50c1aca0ed1c332e2a992109b2efd630869b5
OmKadam989 <omkadam84129@gmail.com>
2025-07-15 22:05:17 +0530

    lec 29

--- Recursion-lV/lec29.cpp
+++ Recursion-lV/lec29.cpp
@@ -1,21 +1,90 @@
-// Robber House
 #include<iostream>
+#include<vector>
 using namespace std;
-void robber(int a[], int &sum,int total, int size, int index)
-{
-	if(size <= index) {
-		sum = max(sum, total);
-		return;
-	}
 
-	robber(a, sum,total, size, index+1);
-	robber(a, sum,total + a[index], size, index+2);
+void permutation(string s, int size,int index, string temp)
+{
+    if(temp.size()!=0)
+    {
+        cout<<temp<<" ";
+    }
+    if(index ==size)
+    {
+        return;
+    }
+        if(temp.size()==0)
+        permutation(s,size,index+1, temp);
+        
+        temp += s[index];
+        permutation(s,size,index+1,temp);
+    
 }
+
 int main() {
-	int arr[]= {2,34,5,66,64};
-	int size = sizeof(arr) / sizeof(arr[0]);
-	int sum = 0;
-	int total=0;
-	robber(arr, sum,total, size, 0);
-	cout<<sum;
+    string s ="ipl";
+    int size= s.size();
+    string temp="";
+    permutation(s,size,0,temp);
 }
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void permutation(string s, int size,int index, string temp)
+{
+    if(temp.size()!=0)
+    {
+        cout<<temp<<" ";
+    }
+    if(index ==size)
+    {
+        return;
+    }
+    
+        if(temp.size()==0)
+        {
+        permutation(s,size,index+1, temp);
+        permutation(s,size,index+1,temp+s[index]);
+        return;
+        }
+        
+        temp += s[index];
+        permutation(s,size,index+1,temp);
+    
+}
+
+int main() {
+    string s ="ipl";
+    int size= s.size();
+    string temp="";
+    permutation(s,size,0,temp);
+}
+class Solution {
+public:
+    void fun(vector<int> &candidates, vector<vector<int>> &ans, vector<int> temp, int target, int sum, int index)
+    {
+        if (index == candidates.size()) {
+            if (sum == target) {
+                ans.push_back(temp);
+            }
+            return;
+        }
+
+        // Not take
+        fun(candidates, ans, temp, target, sum, index + 1);
+
+        // Take
+        if (sum + candidates[index] <= target) {
+            temp.push_back(candidates[index]);
+            fun(candidates, ans, temp, target, sum + candidates[index], index);  // Not index+1, since we can reuse
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        fun(candidates, ans, temp, target, 0, 0);
+        return ans;
+    }
+};
